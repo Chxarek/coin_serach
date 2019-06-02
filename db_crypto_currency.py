27f8bfe0484b69601fc5e03ae597a19b8ddfd0a2
chxarek <oblivionandvoid@gmail.com>
2019-06-02 15:05:19 +0300

    removed useless

--- db_crypto_currency.py
+++ db_crypto_currency.py
@@ -7,12 +7,12 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = 'https://coinmarketcap.com/'
+url = 'https://coinmarketcap.com/'                          #connecting and preparing the source data for further transforms
 html = requests.get(url)
 soup = bs4.BeautifulSoup(html.text, "html.parser")
 
-crypto_dict = []
-cur_price = soup.select('.price, .currency-name-container')
+crypto_dict = []                                              #make some sorting and
+cur_price = soup.select('.price, .currency-name-container')   #create unformatted list of currencies with names and price values
 for name in cur_price:
     curr = name.get_text()
     crypto_dict.append(curr)
@@ -20,15 +20,15 @@ for name in cur_price:
 a, b = [], []
 c, d = {}, {}
 i = 0
-while i < len(crypto_dict):
+while i < len(crypto_dict):                    #adding some format to the list's data:
     if i % 2 != 0:
-        a.append(float(crypto_dict[i][1:]))
+        a.append(float(crypto_dict[i][1:]))     #making list of prices with stripping $-sign from price value
     else:
-        b.append(crypto_dict[i])
+        b.append(crypto_dict[i])                #making list of names
     i += 1
 
-for j in range(len(a)):
-     c = {j+1: {'crypto_name': b[j], 'crypto_price': a[j]}}
+for j in range(len(a)):                                            #creating dictionary out from the previous,
+     c = {j+1: {'crypto_name': b[j], 'crypto_price': a[j]}}        #containing data like: {1: {'crypto_name': 'Bitcoin', 'crypto_price': 8743.36},...}
      d.update(c)
 
 db_name = 'CRYPTO_COIN'
@@ -39,9 +39,9 @@ def upd_db():
 
     conn = sqlite3.connect('crypto.sqlite')
     cur = conn.cursor()
-    cur.execute('DELETE FROM {0}'.format(db_name))
+    cur.execute('DELETE FROM {0}'.format(db_name))      #deleting all the previous data every time we make an update
     for i in range(1, len(d) + 1):
-        cur.execute('INSERT INTO {} ({}, {}) VALUES (?, ?)'.format(db_name, fields[0], fields[1]),
+        cur.execute('INSERT INTO {} ({}, {}) VALUES (?, ?)'.format(db_name, fields[0], fields[1]),      #filling the db with the updated stuff
                     (d[i]['crypto_name'], d[i]['crypto_price']))
     conn.commit()
     cur.close()
--- db_to_json.py
+++ db_to_json.py
@@ -1,7 +1,7 @@
 import sqlite3, re, json
 from db_crypto_currency import db_name, fields, d, upd_db
 
-upd_db()
+upd_db()            #getting the results from db_crypto_currency.py
 
 
 number = int(input('Number of coins: '))
@@ -10,17 +10,17 @@ number = int(input('Number of coins: '))
 def extr_from_db():
     conn = sqlite3.connect('crypto.sqlite')
     cur = conn.cursor()
-    cur.execute('SELECT {3} FROM {0} ORDER BY {2} DESC LIMIT {1}'.format(db_name, number, fields[1], fields[0]))
+    cur.execute('SELECT {3} FROM {0} ORDER BY {2} DESC LIMIT {1}'.format(db_name, number, fields[1], fields[0])) #currencies names selection, ordered by value regression
     a1 = cur.fetchall()
-    cur.execute('SELECT {2} FROM {0} ORDER BY {2} DESC LIMIT {1}'.format(db_name, number, fields[1], fields[0]))
-    a2 = str(cur.fetchall())
-    a2 = re.findall("[0-9.]+", a2)
+    cur.execute('SELECT {2} FROM {0} ORDER BY {2} DESC LIMIT {1}'.format(db_name, number, fields[1], fields[0])) #currencies price values selection, ordered by value regression
+    a2 = str(cur.fetchall())        #tupple to string
+    a2 = re.findall("[0-9.]+", a2)  #only digits remain, emulating float
     l = []
     for i in range(len(a1)):
-        listing = {a1[i][0]: a2[i]}
-        l.append(listing)
-    with open('crypto(dump).json', 'w') as file_dump:
+        listing = {a1[i][0]: a2[i]}                             # getting data like this: {'Bitcoin': '8734.43'}, {'ThoreCoin': '1328.15'}, {'Maker': '727.12'}
+        l.append(listing)                                       # a1[i][0] - getting rid from ',' (tupple type trash)
+    with open('crypto(dump).json', 'w') as file_dump:           # writing copy of data to the file
         json.dump(l, file_dump, indent=2, ensure_ascii=False)
-    return json.dumps(l, separators=('\n', ':'))
+    return json.dumps(l, separators=('\n', ':'))                #formatting to json with column type printing
 
 print(extr_from_db())
